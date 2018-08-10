796550e5de8f9ef2e3845f4235d759a0f421f30c
GakonHD <dang.ton.th@gmail.com>
2018-08-09 20:59:52 -0500

    Initial Commit

---
+++ IgnitePOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IgnitePOC
+//
+//  Created by Dang Ton on 8/9/18.
+//  Copyright © 2018 Dang Ton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
